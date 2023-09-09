e95d1a1769e71f0f73e01459dd84fa995bc97dd3
AnitaToRemember <best.anac@gmail.com>
2023-09-09 16:06:49 +0200

    <adding routes to navigation bar>

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,7 +7,7 @@ const NavigationBar = () => {
 				<ul className="nav-list">
 				<li className="nav-item"><Link to="/links">Home</Link></li>
         <li className="nav-item"><Link to="/mylinks">My Links</Link></li>
-        <li className="nav-item"><Link to="/filter">Filter</Link></li>
+        <li className="nav-item"><Link to="/links/filter">Filter</Link></li>
         <li className="nav-item"><Link to="/users">Account</Link></li>
 				</ul>
 			</nav>

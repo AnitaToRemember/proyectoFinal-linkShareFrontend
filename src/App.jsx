e4a732495d1eeaa98d2e5dc4bda703004e56b79e
AnitaToRemember <best.anac@gmail.com>
2023-09-11 12:32:31 +0200

    <routes now working>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import HeaderBar from '../components/HeaderBar'
 import NavigationBar from '../components/NavigationBar'
-import './components/NavigationBar.css'
 
 function HomePage() {
 

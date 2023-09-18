f86e33b3e6ae9c43e1aa043dee70591f4256f580
Evelin Arboleda <evelinarboleda23@gmail.com>
2023-09-18 04:21:55 +0200

    navigation icons are added

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,17 +1,36 @@
-import '../components/NavigationBar.css'
-import { Link } from 'react-router-dom' // Import Link from react-router-dom
+
+import '../components/NavigationBar.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faLink, faFilter, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const NavigationBar = () => {
     return (
         <nav className="navbar">
             <ul className="nav-list">
-                <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item"><Link to="/mylinks">MyLinks</Link></li>
-                <li className="nav-item"><Link to="/filter">Filter</Link></li>
-                <li className="nav-item"><Link to="/account">Account</Link></li>
+                <li className="nav-item">
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faHome} /> Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/mylinks">
+                        <FontAwesomeIcon icon={faLink} /> MyLinks
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/filter">
+                        <FontAwesomeIcon icon={faFilter} /> Filter
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/account">
+                        <FontAwesomeIcon icon={faUser} /> Account
+                    </Link>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
 
-export default NavigationBar
+export default NavigationBar;

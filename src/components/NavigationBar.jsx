b86e9cf00e7c7dd1090e3cb25ebc5e148a2de437
AnitaToRemember <best.anac@gmail.com>
2023-09-18 13:06:31 +0200

    <adding icons to nav bar>

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,14 +1,16 @@
 import '../components/NavigationBar.css'
-import { Link } from 'react-router-dom' // Import Link from react-router-dom
+import { Link } from 'react-router-dom' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faLink, faSort, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const NavigationBar = () => {
     return (
         <nav className="navbar">
             <ul className="nav-list">
-                <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item"><Link to="/mylinks">MyLinks</Link></li>
-                <li className="nav-item"><Link to="/filter">Filter</Link></li>
-                <li className="nav-item"><Link to="/account">Account</Link></li>
+                <li className="nav-item"><Link to="/"><FontAwesomeIcon icon={faHouse} size="2xl" style={{color: "#0093ff",}} /> </Link></li>
+                <li className="nav-item"><Link to="/mylinks"><FontAwesomeIcon icon={faLink} size="2xl"  style={{color: "#0093ff",}} /></Link></li>
+                <li className="nav-item"><Link to="/filter"><FontAwesomeIcon icon={faSort} size="2xl"  style={{color: "#0093ff",}} /></Link></li>
+                <li className="nav-item"><Link to="/account"><FontAwesomeIcon icon={faUser} size="2xl"  style={{color: "#0093ff",}} /></Link></li>
             </ul>
         </nav>
     )

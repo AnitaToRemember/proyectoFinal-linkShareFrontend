f27e85306683fc6f10e47e8055a692a0a3827809
Evelin Arboleda <evelinarboleda23@gmail.com>
2023-09-24 14:58:28 +0200

    show home if user is logged or root if not

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,12 +2,16 @@ import '../components/NavigationBar.css'
 import { Link } from 'react-router-dom' 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faLink, faSort, faUser } from '@fortawesome/free-solid-svg-icons'
+import { AuthContext } from '../context/AuthContext'
+import { useContext } from 'react'
 
 const NavigationBar = () => {
+    const { user } = useContext(AuthContext);
+    const homeLink = (user === undefined || user === null) ? "/" : "/home"
     return (
         <nav className="navbar">
             <ul className="nav-list">
-                <li className="nav-item"><Link to="/"><FontAwesomeIcon icon={faHouse} size="2xl" style={{color: "#0093ff",}} /> </Link></li>
+                <li className="nav-item"><Link to={homeLink}><FontAwesomeIcon icon={faHouse} size="2xl" style={{color: "#0093ff",}} /> </Link></li>
                 <li className="nav-item"><Link to="/mylinks"><FontAwesomeIcon icon={faLink} size="2xl"  style={{color: "#0093ff",}} /></Link></li>
                 <li className="nav-item"><Link to="/filter"><FontAwesomeIcon icon={faSort} size="2xl"  style={{color: "#0093ff",}} /></Link></li>
                 <li className="nav-item"><Link to="/account"><FontAwesomeIcon icon={faUser} size="2xl"  style={{color: "#0093ff",}} /></Link></li>
